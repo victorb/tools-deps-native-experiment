fba5b13655e6be01f1ac921ba8efef95a77ff125
victorbjelkholm <victorbjelkholm@gmail.com>
2021-03-05 12:20:31 +0100

    Poor mans debugging for circleci graalvm builds
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

--- src/borkdude/tdn/main.clj
+++ src/borkdude/tdn/main.clj
@@ -5,10 +5,15 @@
 
 #_(require '[clojure.tools.deps.alpha.extensions :as ext]) ;; somehow requiring this namespace as a side effect helps...
 (require '[clojure.edn :as edn]
+         '[clojure.pprint :refer [pprint]]
          '[clojure.tools.deps.alpha.util.maven :as mvn])
 
 ;; avoid null pointer
 (mvn/make-system)
+
+(defn pp-ret [a]
+  (pprint a)
+  a)
 
 (defn -main [& args]
   (let [arg (first args)
@@ -16,9 +21,13 @@
                   (slurp arg)
                   arg)]
     (prn (-> (edn/read-string edn-str)
+             (pp-ret)
              (update :mvn/repos (fn [repos]
                                   (or repos
                                       {"central" {:url "https://repo1.maven.org/maven2/"}
                                        "clojars" {:url "https://repo.clojars.org/"}})))
+             (pp-ret)
              (deps/resolve-deps nil)
-             (deps/make-classpath nil nil)))))
+             (pp-ret)
+             (deps/make-classpath nil nil)
+             (pp-ret)))))
